71b2cba22509d89d9cfbc7fceb9bd6775d61dd97
Alex <ales.cep@gmail.com>
2023-01-09 12:47:14 +0100

    .gitignore: added .exe file CodeExample/main.c make some changes in main.c file

--- CodeExample/main.c
+++ CodeExample/main.c
@@ -4,19 +4,23 @@
 int main(int argv, char** args) {
 
 	//EXAMPLE OF USE
-
-	CircularBuffer buffer{{'\0'}, -1, -1};
+	CircularBuffer buffer = {.head = -1, .tail = -1 };
 
 	ResponseStatus response_insert = insert_char(&buffer, 'A');
 	if (response_insert != OK) {
-		printf("ERROR after insert\n");
+		printf("ERROR after %s\n", "ResponseStatus insert_char(CircularBuffer* circ_buffer, CUSTOM_TYPE char_to_write)");
 		return -1;
 	}
+	else
+		printf("%s successful\n", "ResponseStatus insert_char(CircularBuffer* circ_buffer, CUSTOM_TYPE char_to_write)");
+
 	char returned_char;
 	ResponseStatus response_get = get_char(&buffer, &returned_char);
 	if (response_get != OK) {
-		printf("ERROR after get\n");
+		printf("ERROR after %s\n", "ResponseStatus get_char(CircularBuffer* circ_buffer, CUSTOM_TYPE* returned_char)");
 		return -1;
 	}
+	else
+		printf("%s successful\n", "ResponseStatus get_char(CircularBuffer* circ_buffer, CUSTOM_TYPE* returned_char)");
 	return 0;
 }
